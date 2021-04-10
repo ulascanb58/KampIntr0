11779274e8a32881dafabf82fa8098b4363e3251
ulascanb58 <ulascanb58@gmail.com>
2021-04-10 15:00:14 +0300

    Add project files.

---
+++ KampIntr0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KampIntr0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
